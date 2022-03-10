9ac98cf39a1cbd2f011718247298d780a99e23b8
Mark Murphy <murphy.mark@northeastern.edu>
2022-03-10 00:23:36 -0500

    Add grid generation in javascript

---
+++ etch_a_sketch.js
@@ -0,0 +1,35 @@
+
+const GRID_WIDTH = 16;
+const GRID_HEIGHT = 16;
+
+const generateCellDiv = function() {
+    const cellDiv = document.createElement("div");
+    return cellDiv;
+}
+
+// -> { rowDiv, [cellDivs] }
+const generateRow = function() {
+    const rowDiv = document.createElement("div");
+    const cellDivs = Array(GRID_WIDTH).fill().map(_ => generateCellDiv());
+    return {rowDiv, cellDivs};
+}
+
+// Attach each row's divs to the grid container
+const attachGridDivs = function(gridContainer, rows) {
+    for (const {rowDiv, cellDivs} of rows) {
+        gridContainer.appendChild(rowDiv);
+        for (const cellDiv of cellDivs) {
+            rowDiv.appendChild(cellDiv);
+        }
+    }
+}
+
+
+function main() {
+    const gridContainer = document.querySelector("#grid_container");
+    const rows = Array(GRID_HEIGHT).fill().map(_ => generateRow());
+
+    attachGridDivs(gridContainer, rows);
+}
+
+main();
