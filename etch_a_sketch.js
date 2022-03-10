7c4566915cb219b8e50bdfdd5ed9787cd813879e
Mark Murphy <murphy.mark@northeastern.edu>
2022-03-10 15:43:40 -0500

    Add reset button

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,6 +1,5 @@
 
-const GRID_WIDTH = 16;
-const GRID_HEIGHT = 16;
+const DEFAULT_GRID_SIZE = 16;
 
 const addCellDivHighlight = function(event) {
     event.target.classList.add("gridCellHighlighted");
@@ -14,16 +13,23 @@ const generateCellDiv = function() {
 }
 
 // -> { rowDiv, [cellDivs] }
-const generateRow = function() {
+const generateRow = function(width) {
     const rowDiv = document.createElement("div");
     rowDiv.classList.add("gridRow");
 
-    const cellDivs = Array(GRID_WIDTH).fill().map(_ => generateCellDiv());
+    const cellDivs = Array(width).fill().map(_ => generateCellDiv());
     return {rowDiv, cellDivs};
+}
+
+// -> [ { rowDiv, [cellDivs] } ]
+const generateGrid = function(size) {
+    const rows = Array(size).fill().map(_ => generateRow(size));
+    return rows;
 }
 
 // Attach each row's divs to the grid container
 const attachGridDivs = function(gridContainer, rows) {
+    gridContainer.innerHTML = "";
     for (const {rowDiv, cellDivs} of rows) {
         gridContainer.appendChild(rowDiv);
         for (const cellDiv of cellDivs) {
@@ -32,12 +38,25 @@ const attachGridDivs = function(gridContainer, rows) {
     }
 }
 
+// Gets the size of the new grid, creates rows, and attaches the new grid rows.
+const resetGrid = function(gridContainer) {
+
+    let gridSize = prompt("How many squares per side?", 16);
+    gridSize = parseInt(gridSize);
+    if (isNaN(gridSize) || gridSize <= 0) gridSize = DEFAULT_GRID_SIZE;
+    if (gridSize > 100) gridSize = 100;
+
+    const rows = generateGrid(gridSize);
+    attachGridDivs(gridContainer, rows);
+}
 
 function main() {
     const gridContainer = document.querySelector("#grid_container");
-    const rows = Array(GRID_HEIGHT).fill().map(_ => generateRow());
-
+    const rows = generateGrid(DEFAULT_GRID_SIZE);
     attachGridDivs(gridContainer, rows);
+
+    const resetButton = document.querySelector("#reset_button");
+    resetButton.addEventListener("click", () => resetGrid(gridContainer))
 }
 
 main();
